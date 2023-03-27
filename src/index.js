120d128d37eb6551ca63d2f36f67c8ad4adf4d77
alina <akupriianova99@gmail.com>
2023-03-27 15:19:04 +0300

    update parsers

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
+import path from 'path';
+import * as fs from 'fs';
 import genDiff from './genDiff.js';
 import parse from './parsers.js';
 import format from './formatters/index.js';
 
+const getTypeFile = (pathFile) => path.extname(pathFile).slice(1);
+const getData = (filepath) => parse(fs.readFileSync(filepath, 'utf-8'), getTypeFile(filepath));
+const buildFullPath = (filepath) => path.resolve(process.cwd(), filepath);
+
 const buildDiff = (filepath1, filepath2, formatName) => {
   // читаем данные из файлов
-  const parsedFile1 = parse(filepath1);
-  const parsedFile2 = parse(filepath2);
+  const parsedFile1 = getData(buildFullPath(filepath1));
+  const parsedFile2 = getData(buildFullPath(filepath2));
 
   // строим дерево по этим данным
   const diff = genDiff(parsedFile1, parsedFile2);
--- src/parsers.js
+++ src/parsers.js
@@ -1,28 +1,6 @@
-/* eslint-disable import/no-extraneous-dependencies */
-import * as fs from 'fs';
 import yaml from 'js-yaml';
-import path from 'path';
-
-const readFile = (filepath) => {
-  const data = fs.readFileSync(filepath, 'utf8');
-  return data;
-};
 
 const parsers = { json: JSON.parse, yaml: yaml.load, yml: yaml.load };
+const getParser = (data, typeFile) => parsers[typeFile](data);
 
-const getParser = (typeFile) => parsers[typeFile];
-
-const parse = (filepath) => {
-  // читаем файл
-  const data = readFile(filepath);
-
-  // узнаем расширение
-  const type = path.extname(filepath);
-
-  // берем функцию для чтения в зависимости от расширения файла
-  const parser = getParser(type);
-
-  return parser(data);
-};
-
-export default parse;
+export default getParser;
